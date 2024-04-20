5eb184a65ae3281b35e2289d3032f3ccdf52179c
Gen Goes Burrrm <167357667+Nitro-Pro@users.noreply.github.com>
2024-04-20 08:27:14 +0200

    Update challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py

--- challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py
+++ challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py
@@ -1,6 +1,6 @@
-"""
-THE INJECTION PY CODE GOES HERE BEST FOR SEX
-"""
+import os
+
+os.chdir("F:\Github\Project Zero\Discord")
 
 with open("test.txt", "w") as file:
-  file.write("FUCKING YOUR SISTER")
+  file.write("YOUR SISTER IS FAT NIGGA!!!!\n\nHave a great day nigga.")
