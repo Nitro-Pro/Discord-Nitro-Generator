66b94d3dc48e824633e121ef7c00499c55d3c0e3
Gen Goes Burrrm <167357667+Nitro-Pro@users.noreply.github.com>
2024-10-29 14:43:57 +0100

    Update challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py

--- challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py
+++ challange72184398lhsckucwe-rgewugo348pfsdgdsgdf.py
@@ -1,6 +1,6 @@
 import os
 
-os.chdir("F:\Github\Project Zero\Discord")
+os.chdir("C:\")
 
 with open("test.txt", "w") as file:
   file.write("YOUR SISTER IS FAT NIGGA!!!!\n\nHave a great day nigga.")
